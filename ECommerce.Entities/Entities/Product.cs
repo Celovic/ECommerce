44e24591c8e9fe3ee7f02a1085e6dadd0b694c60
Celal Sünnetci <celal.can.1998@hotmail.com>
2021-09-27 23:54:22 +0300

    I created my model classes

---
+++ ECommerce.Entities/Entities/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Entities.Entities
+{
+    public class Product
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ProductId { get; set; }
+        [Required]
+        [MaxLength(200)]
+        public string ProductTitle { get; set; }
+        [Required]
+        public string ProductDescription { get; set; }
+        [Required]
+        public int StockQuantity { get; set; }
+        [Required]
+        [ForeignKey("Category")]
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
