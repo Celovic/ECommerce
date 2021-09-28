949335fd52be1c664d4a28ea9d71d94eaab5cde4
Celal Sünnetci <celal.can.1998@hotmail.com>
2021-09-28 09:26:51 +0300

    I created the data access and migration

---
+++ ECommerce.Entities/Context/ECommerceDbContext.cs
@@ -0,0 +1,19 @@
+﻿using ECommerce.Entities.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerce.Entities.Context
+{
+    public class ECommerceDbContext :DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server = DESKTOP-154APPP; Database = ECommerce; Integrated Security = true; Trusted_Connection = True;MultipleActiveResultSets=True");
+        }
+        public DbSet<Product> TBLProduct { get; set; }
+        public DbSet<Category> TBLCategory{ get; set; }
+    }
+}
