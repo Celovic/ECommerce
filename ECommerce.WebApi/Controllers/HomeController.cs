a852d1d8b94035a563b8fc3a6c79105925cde78f
Celal Sünnetci <celal.can.1998@hotmail.com>
2021-10-04 16:40:26 +0300

    I created product,category and home controller

---
+++ ECommerce.WebApi/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerce.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpGet("{productId}")]
+        public string Get(int productId)
+        {
+            return "value";
+        }
+        [HttpGet("{categoryId}")]
+        public string Get(int categoryId)
+        {
+            return "value";
+        }
+
+    }
+}
